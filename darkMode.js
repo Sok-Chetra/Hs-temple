a29755f7be9d6bd7d8728ef21b538677dfed6590
tra <sokchetra5400@gmail.com>
2022-10-23 13:19:07 +0700

    changed darkmode function code but still not checked after refresh and done hide nav when scroll down and hide scroll up

--- darkMode.js
+++ darkMode.js
@@ -1,5 +1,20 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark");
-    }    
+if(localStorage.getItem('theme') == 'dark'){
+    setDarkMode();
 
+    if (document.getElementById('checkbox').checked) {
+        localStorage.setItem('checkbox', true)
+    }
+}
+
+function setDarkMode(){
+    let isDark = document.body.classList.toggle('dark');
+
+    if (isDark) {
+        setDarkMode.checked = true;
+        localStorage.setItem('theme', dark);
+        document.getElementById('checkbox').setAttribute('autocomplete', 'off');
+    } else {
+        setDarkMode.checked = true;
+        localStorage.removeItem('theme', 'dark');
+    }
+}
